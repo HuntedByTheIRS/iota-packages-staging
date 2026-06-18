c2c1a557570f5fb65255d28ab0ccf9277cfb83d8
Specter <dragonmasterax360@gmail.com>
2026-06-18 11:45:25 -0400

    Update Doit (0.0.3) (#25)
    Update recipe.sh aswell

--- packages/doit/recipe.sh
+++ packages/doit/recipe.sh
@@ -1,13 +1,5 @@
 cargo build --release
-if command -v doas >/dev/null 2>&1; then
-  run_elevated="doas"
-elif command -v sudo >/dev/null 2>&1; then
-  run_elevated="sudo"
-else
-  echo "Error: Neither doas nor sudo is installed."
-  exit 1
-fi
 
-$run_elevated cp target/release/doit /usr/local/bin/doit
-$run_elevated chown root:root /usr/local/bin/doit
-$run_elevated chmod u+s /usr/local/bin/doit
+$IOTA_SUPERUSER cp target/release/doit "$IOTA_ROOT_PATH"/doit
+$IOTA_SUPERUSER chown root:root "$IOTA_ROOT_PATH"/doit
+$IOTA_SUPERUSER chmod u+s "$IOTA_ROOT_PATH"/doit
