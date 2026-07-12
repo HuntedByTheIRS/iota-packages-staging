d9851508fafbbb871d82d59057d78fef84610dab
HuntedByTheIRS <huntedbyth3irs@gmail.com>
2026-07-12 05:20:27 -0400

    simplify

--- kernels/patched/zen/zen-latest.sh
+++ kernels/patched/zen/zen-latest.sh
@@ -1,65 +1,15 @@
-prepare() {
-  git clone --branch linux-rolling-stable --single-branch --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git || {
-    echo "git failed"
-    exit 1
-  }
-
-  cd linux || {
-    echo "Could not enter directory"
-    in_linux=false
-    exit 1
-  }
-  in_linux=true
-
-  version=$(git tag)
+git clone --branch linux-rolling-stable --single-branch --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git || {
+  echo "git failed"
+  exit 1
+}
+cd linux || {
+  echo "Could not enter directory"
+  exit 1
 }
 
-patch() {
-  wget "https://github.com/zen-kernel/zen-kernel/releases/download/${version}-zen1/linux-${version}-zen1.patch.zst"
-  unzstd "linux-${version}-zen1.patch.zst"
+version=$(git tag)
 
-  patch -p1 <"linux-${version}-zen1.patch"
-}
+wget "https://github.com/zen-kernel/zen-kernel/releases/download/${version}-zen1/linux-${version}-zen1.patch.zst"
+unzstd "linux-${version}-zen1.patch.zst"
 
-build() {
-  echo "just testing"
-}
-
-install() {
-  echo "reached install"
-}
-
-clean() {
-  if [[ "$(basename "$PWD")" == "linux" ]]; then
-    cd ..
-  fi
-  rm -rf linux/
-  rm "$0"
-}
-
-case "$1" in
-"prepare")
-  prepare
-  ;;
-"patch")
-  if [[ $in_linux == true ]]; then
-    patch
-  fi
-  ;;
-"build")
-  if [[ $in_linux == true ]]; then
-    build
-  fi
-  ;;
-"install")
-  if [[ $in_linux == true ]]; then
-    install
-  fi
-  ;;
-"clean")
-  clean
-  ;;
-*)
-  echo "Usage: $0 {prepare|build|install|clean}"
-  ;;
-esac
+patch -p1 <"linux-${version}-zen1.patch"
