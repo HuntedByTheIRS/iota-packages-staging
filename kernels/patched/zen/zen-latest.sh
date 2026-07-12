bb6a971d6fed2e6eb8afb48536ea4f5f154cd0be
HuntedByTheIRS <huntedbyth3irs@gmail.com>
2026-07-12 05:45:48 -0400

    update

--- kernels/patched/zen/zen-latest.sh
+++ kernels/patched/zen/zen-latest.sh
@@ -1,6 +1,9 @@
+git clone --branch linux-rolling-stable --single-branch --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
+cd linux || exit 1
+
 version=$(make kernelversion)
 
 wget "https://github.com/zen-kernel/zen-kernel/releases/download/v${version}-zen1/linux-v${version}-zen1.patch.zst"
 unzstd "linux-v${version}-zen1.patch.zst"
 
-patch -p1 <"linux-${version}-zen1.patch"
+patch -p1 <"linux-v${version}-zen1.patch"
